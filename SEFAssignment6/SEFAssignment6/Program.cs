15a97c9a35e79f2d14bf71fac11b3ec7a545b25e
Bahar Sadullah <Bsadullah5365@conestogac.on.ca>
2024-04-14 17:38:44 -0400

    First Commit

---
+++ SEFAssignment6/SEFAssignment6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEFAssignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints "Hello World!"
+            System.Console.WriteLine("Hello World!");
+            return;
+        }
+    }
+}
